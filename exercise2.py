940104d2be5f3f35600f765f7c520e439f97dc0d
Straggio <strack.ms@icloud.com>
2022-11-03 20:30:00 +0100

    added function isub to the Point2D class

--- exercise2.py
+++ exercise2.py
@@ -14,6 +14,9 @@ class Point2D:
     def y(self) -> float:
         return self._coordinates[1]
 
+    def __isub__(self, other: Vector) -> Point2D:
+        assert len(other._coordinates) == 2
+        return Point2D(self.x - other._coordinates[0], self.y - other._coordinates[1])
 
 def test_point_construction() -> None:
     point = Point2D(1.0, 42.0)
